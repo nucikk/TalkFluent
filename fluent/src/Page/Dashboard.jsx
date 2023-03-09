05c4df6515b842819745462583ad5a2330550df1
nuci <kirianuci@gmail.com>
2023-03-09 20:35:40 +0400

    Add styled component and design for the main page

--- fluent/src/Page/Dashboard.jsx
+++ fluent/src/Page/Dashboard.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import styled from "styled-components"
 
+
+const Container = styled.div`
+ height: 100vh;
+ background-color: #dbf0b5;
+ overflow: hidden;
+ display: flex;
+ justify-content: center;
+ align-items: center;
+`
+const Center = styled.div`
+ width: 70%;
+ overflow: hidden;
+ background-color: #acf7ff;
+`
+const Section = styled.div`
+  width: 393px;
+  height: 457px;
+  background: linear-gradient(135deg, #353535 0%, #4F4F4F 39%, #DF91F2 100%);
+background-blend-mode: multiply;
+             `
 const Dashboard = () => {
   return (
-    <div>
-      Dashboard
-    </div>
+    <>
+      <Container>
+        <Center>
+          <Section>
+            
+          </Section>
+        </Center>
+      </Container>
+    </>
   )
 }
 
